f9afef45411d2012079d73df1a9eca4906473112
Sharoon <sharoonbabu@gmail.com>
2017-04-16 00:55:14 +0000

    Waste

--- mylstm.py
+++ mylstm.py
@@ -5,95 +5,77 @@ from tflearn.data_utils import to_categorical, pad_sequences
 import sklearn
 from sklearn.model_selection import train_test_split
 import mydata as data
+import tensorflow as tf
 
 def build_model(max_features, maxlen):
-	"""Build LSTM model"""
-	# model = Sequential()
-	# model.add(Embedding(max_features, 128, input_length=maxlen))
-	# model.add(LSTM(128))
-	# model.add(Dropout(0.5))
-	# model.add(Dense(1))
-	# model.add(Activation('sigmoid'))
-
-	# model.compile(loss='binary_crossentropy',
-	#               optimizer='rmsprop')
-
-	net = tflearn.input_data([None, 5548])
-	net = tflearn.embedding(net, input_dim=10000, output_dim=128)
-	net = tflearn.lstm(net, 128, dropout=0.8)
-	net = tflearn.fully_connected(net, 2, activation='softmax')
-	net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
-						 loss='categorical_crossentropy')
-
-	# Training
-	model = tflearn.DNN(net, tensorboard_verbose=0)
-	return model
+		net = tflearn.input_data(shape=[None,73])
+		
+		
+		net = tflearn.embedding(net, input_dim=73, output_dim=128)
+		
+		net = tflearn.lstm(net, 128, dropout=0.8)
+		
+		net = tflearn.fully_connected(net, 1, activation='sigmoid')
+		
+		net = tflearn.regression(net, optimizer='rmsprop', learning_rate=0.001,
+		loss='binary_crossentropy')
+		
+		# Training
+		model = tflearn.DNN(net, tensorboard_verbose=0)
+		
+		return model
 
 
-def run(max_epoch=25, nfolds=10, batch_size=128):
-	"""Run train/test on logistic regression model"""
-	indata = data.get_data()
+def run(max_epoch=25, nfolds=10, batch_size=16):
+		"""Run train/test on logistic regression model"""
+		indata = data.get_data()
+		np.random.shuffle(indata)
+		# Extract data and labels
+		X = [x[1] for x in indata]
+		labels = [x[0] for x in indata]
 
-	# Extract data and labels
-	X = [x[1] for x in indata]
-	labels = [x[0] for x in indata]
+		# Generate a dictionary of valid characters
+		valid_chars = {x:idx+1 for idx, x in enumerate(set(''.join(X)))}
 
-	# Generate a dictionary of valid characters
-	valid_chars = {x:idx+1 for idx, x in enumerate(set(''.join(X)))}
+		max_features = len(valid_chars) + 1
+		maxlen = np.max([len(x) for x in X])
+		X = X[:14000]
+		labels = labels[:14000]
+		
+		# Convert characters to int and pad
+		X = [[valid_chars[y] for y in x] for x in X]
+		X = pad_sequences(X, maxlen=maxlen)
 
-	max_features = len(valid_chars) + 1
-	maxlen = np.max([len(x) for x in X])
-	X = X[:100]
-	labels = labels[:100]
+		# Convert labels to 0-1
+		y = [0 if x == 'benign' else 1 for x in labels]
 
-	# Convert characters to int and pad
-	X = [[valid_chars[y] for y in x] for x in X]
-	X = pad_sequences(X, maxlen=maxlen)
+		X_train = X[:10000]
+		y_train = y[:10000]
+		X_test  = X[12000:14000]
+		y_test  = y[12000:14000]
+		y_train = np.array(y_train)
+		y_train = np.expand_dims(y_train, axis=-1)
+		y_test = np.expand_dims(y_test, axis=-1)
+		print('X_train shape')
+		print (X_train.shape)
+		print ('Y_trina shape')
+		print (len(y_train))
+	    
+		
 
-	# Convert labels to 0-1
-	y = [0 if x == 'benign' else 1 for x in labels]
 
-	final_data = []
-	
-	for fold in range(nfolds):
-		print("fold %u/%u" % (fold+1, nfolds))
-		X_train, X_test, y_train, y_test, _, label_test = train_test_split(X, y, labels, 
-																		   test_size=0.2)
 
 		print('Build model...')
 		model = build_model(max_features, maxlen)
 
 		print("Train...")
-		X_train, X_holdout, y_train, y_holdout = train_test_split(X_train, y_train, test_size=0.05)
-		best_iter = -1
-		best_auc = 0.0
-		out_data = {}
-		X_train = X_train.reshape(1,5548)
-		for ep in range(max_epoch):
-			model.fit(X_train, y_train, batch_size=batch_size,validation_set=(X_test, y_test), show_metric=True, n_epoch=1)
+		model.fit(X_train, y_train, batch_size=batch_size, show_metric=True,n_epoch=3,validation_set=(X_test,y_test))
+		print("Predicting")
+		y_pred = model.predict(X_test)
+		y_pred = np.array(y_pred)
+		y_test = np.array(y_test)
+		# tf.contrib.metrics.accuracy(y_pred, y_test, weights=None)
 
-			t_probs = model.predict(X_holdout)
-			t_auc = sklearn.metrics.roc_auc_score(y_holdout, t_probs)
-
-			print('Epoch %d: auc = %f (best=%f)' % (ep, t_auc, best_auc))
-
-			if t_auc > best_auc:
-				best_auc = t_auc
-				best_iter = ep
-
-				probs = model.predict_proba(X_test)
-
-				out_data = {'y':y_test, 'labels': label_test, 'probs':probs, 'epochs': ep,
-							'confusion_matrix': sklearn.metrics.confusion_matrix(y_test, probs > .5)}
-
-				print(sklearn.metrics.confusion_matrix(y_test, probs > .5))
-			else:
-				# No longer improving...break and calc statistics
-				if (ep-best_iter) > 2:
-					break
-
-		final_data.append(out_data)
-		print('Saving model')
-		model.save('my_model.tflearn')
-
-	return final_data
+if __name__ == '__main__':
+	run()
+			   
--- tf.py
+++ tf.py
@@ -19,7 +19,7 @@ def run_experiments(islstm=True, nfolds=10):
 	if islstm:
 		lstm_results = lstm.run(nfolds=nfolds)
 
-	return lstm_results
+	
 
 
 
@@ -29,56 +29,7 @@ def create_figs(islstm=True, nfolds=10, force=False):
 	if force or (not os.path.isfile(RESULT_FILE)):
 		lstm_results = run_experiments(islstm, nfolds)
 
-		results = {'lstm': lstm_results}
-
-		pickle.dump(results, open(RESULT_FILE, 'w'))
-
-	else:
-		results = pickle.load(open(RESULT_FILE))
 		
-
-		
-	
-
-	if results['lstm']:
-		lstm_results = results['lstm']
-		fpr = []
-		tpr = []
-		for lstm_result in lstm_results:
-			t_fpr, t_tpr, _ = roc_curve(lstm_result['y'], lstm_result['probs'])
-			fpr.append(t_fpr)
-			tpr.append(t_tpr)
-		lstm_binary_fpr, lstm_binary_tpr, lstm_binary_auc = calc_macro_roc(fpr, tpr)
-
-	# Save figure
-	from matplotlib import pyplot as plt
-	with plt.style.context('bmh'):
-		plt.plot(lstm_binary_fpr, lstm_binary_tpr,
-				 label='LSTM (AUC = %.4f)' % (lstm_binary_auc, ), rasterized=True)
-		
-		plt.xlim([0.0, 1.0])
-		plt.ylim([0.0, 1.05])
-		plt.xlabel('False Positive Rate', fontsize=22)
-		plt.ylabel('True Positive Rate', fontsize=22)
-		plt.title('ROC - Binary Classification', fontsize=26)
-		plt.legend(loc="lower right", fontsize=22)
-
-		plt.tick_params(axis='both', labelsize=22)
-		plt.savefig('results.png')
-
-def calc_macro_roc(fpr, tpr):
-	"""Calcs macro ROC on log scale"""
-	# Create log scale domain
-	all_fpr = sorted(itertools.chain(*fpr))
-
-	# Then interpolate all ROC curves at this points
-	mean_tpr = np.zeros_like(all_fpr)
-	for i in range(len(tpr)):
-		mean_tpr += interp(all_fpr, fpr[i], tpr[i])
-
-	return all_fpr, mean_tpr / len(tpr), auc(all_fpr, mean_tpr) / len(tpr)        
-
-
 
 
 
